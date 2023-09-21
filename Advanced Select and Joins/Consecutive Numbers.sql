19120acb33fa735c0fb11015bc76c3425407353b
Venkatesh M <115688026+Venky044@users.noreply.github.com>
2023-09-21 23:03:42 +0530

    Update Consecutive Numbers

--- Advanced Select and Joins/Consecutive Numbers.sql
+++ Advanced Select and Joins/Consecutive Numbers.sql
@@ -46,3 +46,18 @@ from Logs l1
     Logs l2 on l1.id+1 = l2.id and l1.num = l2.num
     inner join 
     Logs l3 on l2.id+1 = l3.id and l2.num = l3.num;
+
+-- or
+-- using window functions lag() & lead()
+with con_nums as (
+    select id, 
+        num as cur_num,
+        lag(num) over(order by id) as pre_num,
+        lead(num) over(order by id) as next_num
+    from Logs
+)
+
+select 
+    distinct(cur_num) as ConsecutiveNums
+from con_nums
+where cur_num = pre_num and cur_num = next_num;
